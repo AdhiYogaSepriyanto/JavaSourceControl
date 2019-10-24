1cbcde2e8549186ba941af4decf7bba05831c50d
rumput.usang <rumput.usang@DESKTOP-24JHQRJ>
2019-10-24 09:52:53 +0700

    Commit Pertama Kali File-File Project

---
+++ SpringFitnessGit/src/main/java/com/example/SpringFitnessGit/SpringFitnessGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringFitnessGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFitnessGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFitnessGitApplication.class, args);
+	}
+
+}
---
+++ SpringFitnessGit/src/test/java/com/example/SpringFitnessGit/SpringFitnessGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringFitnessGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFitnessGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
